60e8f257658404e3b46e4e948b0011753fcc579a
dvlcx <ave.burning.goat.666@gmail.com>
2025-05-30 00:35:30 +0300

    appconfig

---
+++ Sqeeper/Config/Models/AppConfig.cs
@@ -0,0 +1,25 @@
+namespace Sqeeper.Config
+{
+    public class AppConfig
+    {
+        public string Name { get; set; }
+        public string? Type { get; set; }
+        public string Url { get; set; }
+        public string Path { get; set; }
+        public string[]? Query { get; set; }
+        public bool KeepOld { get; set; }
+        public bool IsGithub { get; set; }
+        public string? PostScript { get; set; }
+
+        public AppConfig(string name, string? type, string url, string path, string[]? query, bool keepOld, bool isGithub, string? postScript)
+        {
+            Name = name;
+            Type = type;
+            Path = path;
+            Query = query;
+            KeepOld = keepOld;
+            IsGithub = isGithub;
+            postScript = PostScript;
+        }
+    }
+}
