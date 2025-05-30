e544257da561e6ad531a22c66e081ab712ce9b2d
dvlcx <ave.burning.goat.666@gmail.com>
2025-05-30 22:34:05 +0300

    dont need group string tho

--- Sqeeper/Config/Models/AppConfig.cs
+++ Sqeeper/Config/Models/AppConfig.cs
@@ -1,9 +1,10 @@
+using System.Text.RegularExpressions;
+
 namespace Sqeeper.Config
 {
     public class AppConfig
     {
         public string Name { get; set; }
-        public string? Type { get; set; }
         public string Url { get; set; }
         public string Path { get; set; }
         public string[]? Query { get; set; }
@@ -11,10 +12,9 @@ namespace Sqeeper.Config
         public bool IsGithub { get; set; }
         public string? PostScript { get; set; }
 
-        public AppConfig(string name, string? type, string url, string path, string[]? query, bool keepOld, bool isGithub, string? postScript)
+        public AppConfig(string name, string url, string path, string[]? query, bool keepOld, bool isGithub, string? postScript)
         {
             Name = name;
-            Type = type;
             Url = url;
             Path = path;
             Query = query;
