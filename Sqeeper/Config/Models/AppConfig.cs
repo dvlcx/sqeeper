7be7181ca4d628691932267dd4034414f95f7ac4
dvlcx <ave.burning.goat.666@gmail.com>
2025-06-13 00:08:44 +0300

    antiquery+sourcetype add

--- Sqeeper/Config/Models/AppConfig.cs
+++ Sqeeper/Config/Models/AppConfig.cs
@@ -9,20 +9,30 @@ namespace Sqeeper.Config
         public string Url { get; set; }
         public string Path { get; set; }
         public string[] Query { get; set; }
+        public string[] AntiQuery { get; set; }
         public bool KeepOld { get; set; }
-        public bool IsGithub { get; set; }
+        public UpdateSource SourceType { get; set; }
         public string? PostScript { get; set; }
 
-        public AppConfig(string name, string version, string url, string path, string[] query, bool keepOld, bool isGithub, string? postScript)
+        public AppConfig(string name, string version, string url, string path, string[] query, string[] antiQuery, bool keepOld, UpdateSource sourceType, string? postScript)
         {
             Name = name;
             Version = version;
             Url = url;
             Path = path;
             Query = query;
+            AntiQuery = antiQuery;
             KeepOld = keepOld;
-            IsGithub = isGithub;
+            SourceType = sourceType;
             PostScript = postScript;
         }
+    }
+    
+    public enum UpdateSource
+    {
+        GitHubRelease,
+        GitLabRelease,
+        GitRepository,
+        DirectoryIndex
     }
 }
