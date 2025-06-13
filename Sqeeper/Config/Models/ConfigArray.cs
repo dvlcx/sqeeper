e85a86bc34011a6115d5e9a1ff1fa53f24ee3848
dvlcx <ave.burning.goat.666@gmail.com>
2025-06-13 21:50:43 +0300

    usings rm

--- Sqeeper/Config/Models/ConfigArray.cs
+++ Sqeeper/Config/Models/ConfigArray.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Sqeeper.Config.Models
 {
     public class ConfigArray : IConfigArray
